f53406728ac1c235f718a1a60a64f497a5aea813
Isaac <isaacmorneau@gmail.com>
2018-01-20 17:37:09 -0800

    finalized test and raw code

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,12 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <fcntl.h>
+#include <cmath>
 #include <semaphore.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
+#include <sys/sysinfo.h>
+#include <gmpxx.h>
 
 #define OMP_MODE 1
 #define THREAD_MODE 2
@@ -15,21 +18,51 @@
 
 static inline void print_help() {
     puts("usage:\n"
-            "\t -[-o]penmp                      - run task with openmp\n"
-            "\t -[-p]rocess     <number to use> - run task with manual processes\n"
-            "\t -[-t]hread      <number to use> - run task with manual threads\n"
-            "\t -[-b]ranch      <default 4>     - number of times to run the task\n"
-            "\t -[-i]tterations <default 1>     - number of times to check the run time\n"
-            "\t -[-h]elp                        - this message\n"
+            "\t -[-o]penmp                    - run task with openmp\n"
+            "\t -[-p]rocess                   - run task with processes\n"
+            "\t -[-t]hread                    - run task with threads\n"
+            "\t -[-b]ranch      <default 100> - number of times to calculate pi\n"
+            "\t -[-i]tterations <default 1>   - number of times to run to get time samples\n"
+            "\t -[-h]elp                      - this message\n"
             );
 };
 static int mode = 0;
-static int branches = 4;
-static int splits = 0;
+static int branches = 100;
+static int splits = get_nprocs();
+
+
+void agm(mpf_class& rop1, mpf_class& rop2, const mpf_class& op1,
+        const mpf_class& op2) {
+    rop1 = (op1 + op2) / 2;
+    rop2 = op1 * op2;
+    mpf_sqrt(rop2.get_mpf_t(), rop2.get_mpf_t());
+}
 
 void task() {
-    sleep(1);
+    mpf_set_default_prec(300000);
+    mpf_class x0, y0, resA, resB, Z;
+
+    x0 = 1;
+    y0 = 0.5;
+    Z  = 0.25;
+    mpf_sqrt(y0.get_mpf_t(), y0.get_mpf_t());
+
+    int n = 1;
+    for (int i = 0; i < 16; i++) {
+        agm(resA, resB, x0, y0);
+        Z -= n * (resA - x0) * (resA - x0);
+        n *= 2;
+
+        agm(x0, y0, resA, resB);
+        Z -= n * (x0 - resA) * (x0 - resA);
+        n *= 2;
+    }
+
+    x0 = x0 * x0 / Z;
+    //only print this if you want to see pi a bunch of times
+    //gmp_printf ("%.100000Ff\n", x0.get_mpf_t());
 }
+
 
 //openmp testing
 struct timespec openmp() {
@@ -42,18 +75,10 @@ struct timespec openmp() {
     return {end.tv_sec - start.tv_sec, end.tv_nsec - start.tv_nsec};
 }
 
-//threads testing
 void run_task_proc() {
     for (int i = 0; i < branches/splits; ++i)
         task();
     exit(0);
-}
-
-//threads testing
-void *run_task_thread(void * ) {
-    for (int i = 0; i < branches/splits; ++i)
-        task();
-    return 0;
 }
 
 struct timespec process() {
@@ -83,6 +108,11 @@ struct timespec process() {
 }
 
 
+void *run_task_thread(void * ) {
+    for (int i = 0; i < branches/splits; ++i)
+        task();
+    return 0;
+}
 struct timespec threads() {
     struct timespec start, end;
     pthread_t * th_id = (pthread_t *)malloc(sizeof(pthread_t)*splits);
@@ -112,16 +142,16 @@ int main(int argc, char ** argv) {
         int option_index = 0;
         static int c = 0;
         static struct option long_options[] = {
-            {"branch",      required_argument, 0, 'b' },
-            {"openmp",      no_argument,       0, 'o' },
-            {"process",     required_argument, 0, 'p' },
-            {"thread",      required_argument, 0, 't' },
-            {"itterations", required_argument, 0, 'i' },
-            {"help",        no_argument,       0, 'h' },
-            {0,             0,                 0, 0 }
+            {"branch",      no_argument, 0, 'b' },
+            {"openmp",      no_argument, 0, 'o' },
+            {"process",     no_argument, 0, 'p' },
+            {"thread",      no_argument, 0, 't' },
+            {"itterations", no_argument, 0, 'i' },
+            {"help",        no_argument, 0, 'h' },
+            {0,             0,           0, 0 }
         };
 
-        c = getopt_long(argc, argv, "op:t:b:i:h",
+        c = getopt_long(argc, argv, "optb:i:h",
                 long_options, &option_index);
         if (c == -1)
             break;
@@ -137,11 +167,9 @@ int main(int argc, char ** argv) {
                 mode = OMP_MODE;
                 break;
             case 'p':
-                splits = atoi(optarg);
                 mode = PROC_MODE;
                 break;
             case 't':
-                splits = atoi(optarg);
                 mode = THREAD_MODE;
                 break;
             case 'h':
@@ -156,6 +184,7 @@ int main(int argc, char ** argv) {
         return 1;
     }
 
+    printf("%s seconds:\n", mode == OMP_MODE ? "OpenMP" : mode == PROC_MODE ? "Processes" : "Threads");
     while (itterations--) {
         switch (mode) {
             case OMP_MODE:
@@ -170,7 +199,7 @@ int main(int argc, char ** argv) {
                     return 0;
                 break;
         }
-        printf("%lf seconds\n", elapsed.tv_sec + (elapsed.tv_nsec * 0.0000000001));
+        printf("%g\n", elapsed.tv_sec + (elapsed.tv_nsec * 1e-9));
     }
     return 0;
 }
