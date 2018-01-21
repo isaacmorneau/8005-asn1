c7e1811f3faed51d90d66435e183a51af6794d00
Isaac <isaacmorneau@gmail.com>
2018-01-20 18:08:21 -0800

    finished commenting code

--- src/main.cpp
+++ src/main.cpp
@@ -16,56 +16,78 @@
 #define THREAD_MODE 2
 #define PROC_MODE 3
 
-static inline void print_help() {
+static int mode = 0;
+static int branches = 100;
+static int splits = get_nprocs();
+
+/*
+ * Author & Designer: Isaac Morneau
+ * Date: 10-01-2018
+ * Function: print_help
+ * Paramaters: void
+ * Return: void
+ * Notes: prints the usage information
+ * */
+static inline void print_help(void) {
     puts("usage:\n"
             "\t -[-o]penmp                    - run task with openmp\n"
             "\t -[-p]rocess                   - run task with processes\n"
             "\t -[-t]hread                    - run task with threads\n"
             "\t -[-b]ranch      <default 100> - number of times to calculate pi\n"
             "\t -[-i]tterations <default 1>   - number of times to run to get time samples\n"
-            "\t -[-h]elp                      - this message\n"
+            "\t -[-h]elp                      - this message"
             );
 };
-static int mode = 0;
-static int branches = 100;
-static int splits = get_nprocs();
 
-
-void agm(mpf_class& rop1, mpf_class& rop2, const mpf_class& op1,
-        const mpf_class& op2) {
+/*
+ * Author & Designer: Isaac Morneau
+ * Date: 20-01-2018
+ * Function: agm
+ * Notes: source https://rosettacode.org/wiki/Arithmetic-geometric_mean/Calculate_Pi#C.2B.2B
+ * */
+void agm(mpf_class& rop1, mpf_class& rop2, const mpf_class& op1, const mpf_class& op2) {
     rop1 = (op1 + op2) / 2;
     rop2 = op1 * op2;
     mpf_sqrt(rop2.get_mpf_t(), rop2.get_mpf_t());
 }
 
-void task() {
+/*
+ * Author & Designer: Isaac Morneau
+ * Date: 10-01-2018
+ * Function: task
+ * Paramaters: void
+ * Return: void
+ * Notes: source https://rosettacode.org/wiki/Arithmetic-geometric_mean/Calculate_Pi#C.2B.2B
+ *      used to caluclate pi and is called as the processor benchmark function
+ * */
+void task(void) {
     mpf_set_default_prec(300000);
     mpf_class x0, y0, resA, resB, Z;
-
     x0 = 1;
     y0 = 0.5;
     Z  = 0.25;
     mpf_sqrt(y0.get_mpf_t(), y0.get_mpf_t());
-
     int n = 1;
     for (int i = 0; i < 16; i++) {
         agm(resA, resB, x0, y0);
         Z -= n * (resA - x0) * (resA - x0);
         n *= 2;
-
         agm(x0, y0, resA, resB);
         Z -= n * (x0 - resA) * (x0 - resA);
         n *= 2;
     }
-
     x0 = x0 * x0 / Z;
-    //only print this if you want to see pi a bunch of times
-    //gmp_printf ("%.100000Ff\n", x0.get_mpf_t());
 }
 
-
-//openmp testing
-struct timespec openmp() {
+/*
+ * Author & Designer: Isaac Morneau
+ * Date: 10-01-2018
+ * Function: openmp
+ * Paramaters: void
+ * Return: struct timespec - how long the operation took
+ * Notes: will run the task as many times as branch specifed with OpenMP
+ * */
+struct timespec openmp(void) {
     struct timespec start, end;
     clock_gettime(CLOCK_REALTIME, &start);
 #pragma omp parallel for
@@ -75,67 +97,85 @@ struct timespec openmp() {
     return {end.tv_sec - start.tv_sec, end.tv_nsec - start.tv_nsec};
 }
 
-void run_task_proc() {
-    for (int i = 0; i < branches/splits; ++i)
-        task();
-    exit(0);
-}
-
-struct timespec process() {
+/*
+ * Author & Designer: Isaac Morneau
+ * Date: 10-01-2018
+ * Function: process
+ * Paramaters: void
+ * Return: struct timespec - how long the operation took
+ * Notes: will run the task as many times as branch specifed with threads
+ * */
+struct timespec process(void) {
     struct timespec start, end;
-    //start the clock
     clock_gettime(CLOCK_REALTIME, &start);
     for (int i = 0; i < splits; ++i) {
         switch(fork()) {
-            case 0:
-                //child
-                run_task_proc();
+            case 0: //child
+                for (int i = 0; i < branches/splits; ++i)
+                    task();
+                exit(0);
                 return {-1,-1};
-            case -1:
-                //error
+            case -1: //error
                 perror("fork");
                 return {-1,-1};
-            default:
-                //parent
+            default: //parent
                 break;
         }
     }
-    //spinlock waiting for children to die
     while (wait(0) != -1);
-    //stop the clock
     clock_gettime(CLOCK_REALTIME, &end);
     return {end.tv_sec - start.tv_sec, end.tv_nsec - start.tv_nsec};
 }
 
-
-void *run_task_thread(void * ) {
+/*
+ * Author & Designer: Isaac Morneau
+ * Date: 10-01-2018
+ * Function: run_task_thread
+ * Paramaters: void * - unused
+ * Return: void* - unused
+ * Notes: the function for running a thread task
+ * */
+void *run_task_thread(void *) {
     for (int i = 0; i < branches/splits; ++i)
         task();
     return 0;
 }
+
+/*
+ * Author & Designer: Isaac Morneau
+ * Date: 10-01-2018
+ * Function: threads
+ * Paramaters: void
+ * Return: struct timespec - how long the operation took
+ * Notes: will run the task as many times as branch specifed with threads
+ * */
 struct timespec threads() {
     struct timespec start, end;
+    void * ret;
     pthread_t * th_id = (pthread_t *)malloc(sizeof(pthread_t)*splits);
     pthread_attr_t attr;
     pthread_attr_init(&attr);
-
-    //start the clock
     clock_gettime(CLOCK_REALTIME, &start);
-    for(int i = 0; i < splits; ++i) {
+    for(int i = 0; i < splits; ++i)
         pthread_create(th_id + splits, &attr, run_task_thread, 0);
-    }
     for(int i = 0; i < splits; ++i) {
-        void * ret;
         pthread_join(th_id[splits], &ret);
     }
-    //stop the clock
     clock_gettime(CLOCK_REALTIME, &end);
     pthread_attr_destroy(&attr);
     return {end.tv_sec - start.tv_sec, end.tv_nsec - start.tv_nsec};
 }
 
+/*
+ * Author & Designer: Isaac Morneau
+ * Date: 10-01-2018
+ * Function: main
+ * Paramaters: int argc - the number of paramaters
+ *      char ** argv- the paramater strings
+ * Return: int - 0 for success, otherwise error code
+ * Notes: sets up the argument parsing, initializes the task type and runs itterations
+ * */
 int main(int argc, char ** argv) {
-
     struct timespec elapsed;
     int itterations = 1;
     while (1) {
@@ -150,12 +190,9 @@ int main(int argc, char ** argv) {
             {"help",        no_argument, 0, 'h' },
             {0,             0,           0, 0 }
         };
-
-        c = getopt_long(argc, argv, "optb:i:h",
-                long_options, &option_index);
+        c = getopt_long(argc, argv, "optb:i:h", long_options, &option_index);
         if (c == -1)
             break;
-
         switch (c) {
             case 'b':
                 branches = atoi(optarg);
@@ -183,7 +220,6 @@ int main(int argc, char ** argv) {
         print_help();
         return 1;
     }
-
     printf("%s seconds:\n", mode == OMP_MODE ? "OpenMP" : mode == PROC_MODE ? "Processes" : "Threads");
     while (itterations--) {
         switch (mode) {
